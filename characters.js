e854485a4b8fe70a49e01986c481db51ea3f639a
Juan Bezanilla <juanbezanillaz@gmail.com>
2023-06-09 15:16:18 -0400

    tabla dinámica con APIs

---
+++ characters.js
@@ -0,0 +1,259 @@
+export const characters = [
+    // {
+    //     "id": 1,
+    //     "name": "Rick Sanchez",
+    //     "status": "Alive",
+    //     "species": "Human",
+    //     "type": "",
+    //     "gender": "Male",
+    //     "origin": {
+    //         "name": "Earth (C-137)",
+    //         "url": "https://rickandmortyapi.com/api/location/1"
+    //     },
+    //     "location": {
+    //         "name": "Citadel of Ricks",
+    //         "url": "https://rickandmortyapi.com/api/location/3"
+    //     },
+    //     "image": "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    //     "episode": [
+    //         "https://rickandmortyapi.com/api/episode/1",
+    //         "https://rickandmortyapi.com/api/episode/2",
+    //         "https://rickandmortyapi.com/api/episode/3",
+    //         "https://rickandmortyapi.com/api/episode/4",
+    //         "https://rickandmortyapi.com/api/episode/5",
+    //         "https://rickandmortyapi.com/api/episode/6",
+    //         "https://rickandmortyapi.com/api/episode/7",
+    //         "https://rickandmortyapi.com/api/episode/8",
+    //         "https://rickandmortyapi.com/api/episode/9",
+    //         "https://rickandmortyapi.com/api/episode/10",
+    //         "https://rickandmortyapi.com/api/episode/11",
+    //         "https://rickandmortyapi.com/api/episode/12",
+    //         "https://rickandmortyapi.com/api/episode/13",
+    //         "https://rickandmortyapi.com/api/episode/14",
+    //         "https://rickandmortyapi.com/api/episode/15",
+    //         "https://rickandmortyapi.com/api/episode/16",
+    //         "https://rickandmortyapi.com/api/episode/17",
+    //         "https://rickandmortyapi.com/api/episode/18",
+    //         "https://rickandmortyapi.com/api/episode/19",
+    //         "https://rickandmortyapi.com/api/episode/20",
+    //         "https://rickandmortyapi.com/api/episode/21",
+    //         "https://rickandmortyapi.com/api/episode/22",
+    //         "https://rickandmortyapi.com/api/episode/23",
+    //         "https://rickandmortyapi.com/api/episode/24",
+    //         "https://rickandmortyapi.com/api/episode/25",
+    //         "https://rickandmortyapi.com/api/episode/26",
+    //         "https://rickandmortyapi.com/api/episode/27",
+    //         "https://rickandmortyapi.com/api/episode/28",
+    //         "https://rickandmortyapi.com/api/episode/29",
+    //         "https://rickandmortyapi.com/api/episode/30",
+    //         "https://rickandmortyapi.com/api/episode/31",
+    //         "https://rickandmortyapi.com/api/episode/32",
+    //         "https://rickandmortyapi.com/api/episode/33",
+    //         "https://rickandmortyapi.com/api/episode/34",
+    //         "https://rickandmortyapi.com/api/episode/35",
+    //         "https://rickandmortyapi.com/api/episode/36",
+    //         "https://rickandmortyapi.com/api/episode/37",
+    //         "https://rickandmortyapi.com/api/episode/38",
+    //         "https://rickandmortyapi.com/api/episode/39",
+    //         "https://rickandmortyapi.com/api/episode/40",
+    //         "https://rickandmortyapi.com/api/episode/41",
+    //         "https://rickandmortyapi.com/api/episode/42",
+    //         "https://rickandmortyapi.com/api/episode/43",
+    //         "https://rickandmortyapi.com/api/episode/44",
+    //         "https://rickandmortyapi.com/api/episode/45",
+    //         "https://rickandmortyapi.com/api/episode/46",
+    //         "https://rickandmortyapi.com/api/episode/47",
+    //         "https://rickandmortyapi.com/api/episode/48",
+    //         "https://rickandmortyapi.com/api/episode/49",
+    //         "https://rickandmortyapi.com/api/episode/50",
+    //         "https://rickandmortyapi.com/api/episode/51"
+    //     ],
+    //     "url": "https://rickandmortyapi.com/api/character/1",
+    //     "created": "2017-11-04T18:48:46.250Z"
+    // },
+
+          {
+              "id": 1,
+              "name": "Krusty el payaso ",
+              "status": "Herschel Schmoeckel Pinchas Yerucham Krustofsky (apellido que también se escribe Krustofski ) es el cínico, quemado y plagado de adicciones payaso presentador del programa de televisión favorito de Bart y Lisa",
+              "Imagen": "https://res.cloudinary.com/dglqojivj/image/upload/v1682559693/simpsons/250px-Krusty_the_Clown_nz1z1o.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Personalidad de televisión",
+              "updatedAt": "2023-03-13T00:30:55.514Z"
+          },
+          {
+              "id": 2,
+              "name": "Cletus",
+              "Historia": "Cletus Del Roy Montfort Bigglesworth Spuckler , también conocido como Cletus the Slack-Jawed Yokel , es un campesino sureño estereotípico con una personalidad afable, es delgado y generalmente se lo representa con un par de jeans. Reside con su familia en la Ruta Rural 9 en Springfield .",
+              "Imagen": "https://res.cloudinary.com/dglqojivj/image/upload/v1682559693/simpsons/250px-Cletus_Spuckler_rvpr9g.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Granjero, trabajador de la construcción"
+          },
+          {
+              "_id": 3,
+              "name": "Kirk Van Houten",
+              "Historia": "Kirk Evelyn Van Houten es el padre desempleado de Milhouse y el esposo vuelto a casar de Luann.",
+              "Imagen": "https://static.simpsonswiki.com/images/2/20/Kirk_Van_Houten.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Desempleado"
+          },
+          {
+              "_id": 4,
+              "name": "Richard",
+              "Historia": "Richard es un estudiante canoso de la Escuela Primaria Springfield",
+              "Imagen": "https://i.pinimg.com/originals/d5/7b/49/d57b49e8829b1fcd00b237c7ebde6f4a.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Alumno Escuela Primaria Springfield"
+          },
+          {
+              "id": 5,
+              "name": "Sarah Wiggum",
+              "Historia": "Sarah Wiggum ( de soltera Kanneke  ) es la suave esposa de Jefe Gorgory y madre de Ralph . Se la ve con mayor frecuencia en multitudes o en reuniones como grupos de caridad organizados por Marge",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/2/2b/Sarah_Wiggum.png/150px-Sarah_Wiggum.png",
+              "Genero": "Femenino",
+              "Estado": "Vivo",
+              "Ocupacion": "Ama de casa",
+              "updatedAt": "2023-03-20T01:58:24.912Z"
+          },
+          {
+              "id": 6,
+              "name": "Sideshow Raheem",
+              "Historia": "Sideshow Raheem , también conocido como Raheem Banjoko (née Russell Mason ) fue el compañero de Krusty el Payaso antes de Sideshow Bob . Es un devoto seguidor del Islam",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/0/02/SideshowRaheem.png/200px-SideshowRaheem.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Asistente de Krusty"
+          },
+          {
+              "id": 7,
+              "name": "Jefe de la tribu  ",
+              "Historia": "El Jefe Tribal es el gerente del Caesar's Pow-Wow Casino .",
+              "Imagen": "https://static.simpsonswiki.com/images/c/ca/Tapped_Out_Tribal_Chief.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Gerente del Casino Caesar's Pow-Wow",
+              "updatedAt": "2023-03-08T23:48:53.206Z"
+          },
+          {
+              "id": 8,
+              "name": "Beldad",
+              "Historia": "Belle es una empresaria y propietaria de una casa burlesca en Springfield llamada \" La Maison Derrière \".",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/5/53/Belle.png/200px-Belle.png",
+              "Genero": "Femenino",
+              "Estado": "Vivo",
+              "Ocupacion": "Propietario de La Maison Derriere"
+          },
+          {
+              "id": 9,
+              "name": "Satán con aspecto de Moe",
+              "Historia": "El Satán con aspecto de Moe es el gobernante del infierno que se parece a Moe .",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/5/5c/Moe-looking_Satan.png/250px-Moe-looking_Satan.png",
+              "Genero": "Masculino",
+              "Estado": "Desconocido",
+              "Ocupacion": "Gobernante del infierno"
+          },
+          {
+              "id": 10,
+              "name": "Abraham Simpson I",
+              "Historia": "Abraham Simpson es hijo de Mabel Simpson y Virgil Simpson . Presumiblemente, Abraham recibió su nombre de Abraham Lincoln, quien ayudó directamente a Virgil y Mabel a escapar de la persecución de los esclavistas. Abraham es también el bisabuelo de Abraham \"Abuelo\" Simpson y presumiblemente su homónimo.",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/6/6d/Abraham_Simpson_I.png/200px-Abraham_Simpson_I.png",
+              "Genero": "Masculino",
+              "Estado": "Fallecido",
+              "Ocupacion": "Desconocido"
+          },
+          {
+              "id": 11,
+              "name": "Quetzelica ",
+              "Historia": "Quetzelica es un superhéroe y parte de los Vindicadores . Apareció en Vindicators: Crystal War ",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/f/f7/Quetzelica.png/200px-Quetzelica.png",
+              "Genero": "Femenino",
+              "Estado": "Ficticio",
+              "Ocupacion": "superhéroe"
+          },
+          {
+              "id": 12,
+              "name": "Lugash ",
+              "Historia": "Lugash es instructor de gimnasia. Tiene un fuerte acento rumano y un gran bigote castaño rizado.",
+              "Imagen": "https://static.simpsonswiki.com/images/f/f4/Lugash.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Instructor de Gimnasia"
+          },
+          {
+              "_id": "63e474ae7de1feab2942679e",
+              "name": "Scrat ",
+              "Historia": "Scrat es una rata-ardilla con dientes de sable.",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/7/7f/Scrat.png/250px-Scrat.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Mascota"
+          },
+          {
+              "_id": "63e4775c7de1feab294269b4",
+              "name": "Manjula Nahasapeemapetilon",
+              "Historia": "Manjula Nahasapeemapetilon es la esposa de Apu y madre de sus octillizos ",
+              "Imagen": "https://static.simpsonswiki.com/images/2/28/Manjula_Nahasapeemapetilon.png",
+              "Genero": "Femenino",
+              "Estado": "Vivo",
+              "Ocupacion": "Ama de casa Empleado de Kwik-E-Mart a tiempo parcial"
+          },
+          {
+              "_id": "63e477937de1feab294269de",
+              "name": "Sanjay Nahasapeemapetilon",
+              "Historia": "Sanjay Nahasapeemapetilon es el hermano de Apu .",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/4/46/Sanjay_Nahasapeemapetilon.png/250px-Sanjay_Nahasapeemapetilon.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Trabajador de Kwik-E-Mart"
+          },
+          {
+              "_id": "63e479d77de1feab29426b94",
+              "name": "Ayudante de Santa / Huesos",
+              "Historia": "El pequeño ayudante de Santa , también conocido como No. 8 o Su Majestad, King Suds McDuff de Sixpakistan, es el perro de la familia Simpson . El es un galgo.",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/2/2c/Santa%27s_Little_Helper.png/200px-Santa%27s_Little_Helper.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Mascota",
+              "updatedAt": "2023-03-19T22:20:02.522Z"
+          },
+          {
+              "_id": "63e47ce47de1feab29426dbe",
+              "name": "Steve Friedman",
+              "Historia": "Steve Friedman , también conocido como Jeremy Friedman , adolescente con voz chillona , ​​adolescente con cara de granos y Old Man Peterson , es uno de los pocos adolescentes en Springfield y está perpetuamente atrapado en una serie de trabajos sin salida. Steve tiene acné y su voz siempre está en proceso de quebrarse.",
+              "Imagen": "https://static.simpsonswiki.com/images/1/19/Squeaky-voiced_teen.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Varios"
+          },
+          {
+              "_id": "63e4813a7de1feab294270db",
+              "name": "Annika Van Houten",
+              "Historia": "Annika Van Houten es la prima holandesa de Milhouse .",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/8/85/Annika_Van_Houten.png/200px-Annika_Van_Houten.png",
+              "Genero": "Femenino",
+              "Estado": "Vivo",
+              "Ocupacion": "Desconocido"
+          },
+          {
+              "_id": "63e336d5edf49032166d847a",
+              "name": "Carl Carlson",
+              "Historia": "Carlton Carl Carlson trabaja en la planta de energía nuclear de Springfield . Junto con Lenny , Carl no es solo el compañero de trabajo de Homer (a veces identificado como el supervisor de Homer) en la planta de energía nuclear de Springfield . Homer ha sido uno de sus mejores amigos desde la infancia, a la par de Lenny y detrás de Barney Gumble , y le gusta llamarse a sí mismo un Lenny urbano. También asistió a la Universidad de Springfield A&M.",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/a/a9/Carl_Carlson.png/200px-Carl_Carlson.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Trabajador de la planta de energía nuclear de Springfield"
+          },
+          {
+              "_id": "63e337b0edf49032166d92c7",
+              "name": "Sam",
+              "Historia": "Sam es una de las dos moscas del bar que se suelen ver en Moe's Tavern . Prácticamente no se sabe nada de él. Sam siempre usa una gorra y anteojos y Moe le disparó una vez , pero ha aparecido desde entonces. Sam es un evasor de esposas, aunque Homer todavía habla bien de él.",
+              "Imagen": "https://static.simpsonswiki.com/images/thumb/9/94/Sam.png/150px-Sam.png",
+              "Genero": "Masculino",
+              "Estado": "Vivo",
+              "Ocupacion": "Desconocida"
+          }
+      ]
